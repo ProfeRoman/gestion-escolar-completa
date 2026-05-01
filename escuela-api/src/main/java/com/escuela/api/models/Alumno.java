3bde25b2ec3c33e04447c5bace407e509d4669f8
ProfeRoman <rscollo@hotmail.com>
2026-05-01 15:50:20 -0300

    Agregadas anotaciones Entity y Table en Alumno

--- escuela-api/src/main/java/com/escuela/api/models/Alumno.java
+++ escuela-api/src/main/java/com/escuela/api/models/Alumno.java
@@ -1,6 +1,12 @@
 package com.escuela.api.models;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "alumnos")
